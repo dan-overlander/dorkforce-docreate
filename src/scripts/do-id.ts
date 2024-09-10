e0250417d7af442127757259ca1679785447e209
Dan Overlander <45434163+dan-overlander@users.noreply.github.com>
2024-09-10 21:18:51 +0000

    feat: published package

--- src/scripts/do-id.ts
+++ src/scripts/do-id.ts
@@ -1,10 +1,14 @@
 import { doRandom } from "./do-random";
 
-export const doId = (custom: string = "") => {
-    let customId = custom;
-    if (customId) {
-        customId = customId.replace(/[^\w\s]/gi, '');
-        customId = customId.replace(/ /g, ``);
-    }
-    return `${custom ? customId : null || ["a", "b", "c", "d", "e"][doRandom(0, 5)]}-${doRandom()}`;
+export const doId = (custom = "") => {
+  let newId = "";
+  const notSupplied = custom === "";
+  if (notSupplied) {
+    newId = ["a", "b", "c", "d", "e"][doRandom(0, 5)];
+    newId = `${newId}-${doRandom()}`;
+  } else {
+    newId = custom.replace(/[^\w\s]/gi, "");
+    newId = newId.replace(/ /g, ``);
+  }
+  return newId;
 };
